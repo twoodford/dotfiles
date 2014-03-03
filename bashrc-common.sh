ae214af3c541f009a7409c399f7038be1aaa8fd7
Timothy Woodford <timothy@tux.home>
2014-03-03 17:30:01 -0500

    Correct $TERM in condition to check if we're in X

--- bashrc-common.sh
+++ bashrc-common.sh
@@ -3,7 +3,7 @@ if [[ "$osname" = "Darwin" ]]; then
     alias gvim="mvim"
     alias vi="mvim"
 elif [[ "$osname" = "Linux" ]]; then
-    if [[ "$TERM" = "xterm-256color" ]]; then
+    if [[ "$TERM" = "xterm" ]]; then
         alias vi="gvim"
     fi
 fi
