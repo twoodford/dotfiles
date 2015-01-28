afe3acc705b984b21e1ca7693cd7f1f6ff61e8c7
Timothy Woodford <tim_woodford@aol.com>
2015-01-28 16:26:07 -0500

    Add a nice $PS1 everywhere but Gentoo (which already has it)

--- bashrc-common.sh
+++ bashrc-common.sh
@@ -7,3 +7,7 @@ elif [[ "$osname" = "Linux" ]]; then
         alias vi="gvim"
     fi
 fi
+
+if [[ ! -f /etc/portage ]]; then
+    export PS1="\$(if [[ \$? == 0 ]]; then echo \"\[\033[01;32m\]\342\234\223\"; else echo \"\[\033[01;31m\]\342\234\227\"; fi) $(if [[ ${EUID} == 0 ]]; then echo '\[\033[01;31m\]\h'; else echo '\[\033[01;32m\]\u@\h';fi)\[\033[01;34m\] \W \$\[\033[00m\] "
+fi
