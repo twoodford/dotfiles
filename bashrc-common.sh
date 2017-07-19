1c743f7d4c04026be5c1e38b718de950ebf3d35b
Tim Woodford <timthewoodford@gmail.com>
2017-07-19 09:41:54 -0400

    Add convenience functions with weather and Latex

--- bashrc-common.sh
+++ bashrc-common.sh
@@ -11,3 +11,11 @@ fi
 if [[ ! -f /etc/portage ]]; then
     export PS1="\$(if [[ \$? == 0 ]]; then echo \"\[\033[01;32m\]\342\234\223\"; else echo \"\[\033[01;31m\]\342\234\227\"; fi) $(if [[ ${EUID} == 0 ]]; then echo '\[\033[01;31m\]\h'; else echo '\[\033[01;32m\]\u@\h';fi)\[\033[01;34m\] \W \$\[\033[00m\] "
 fi
+
+function weather() {
+    curl -s http://wttr.in | sed '$d' | sed '$d'
+}
+
+function lmk() {
+    latexmk -pdf -pvc -outdir=$1-out $1.tex
+}
